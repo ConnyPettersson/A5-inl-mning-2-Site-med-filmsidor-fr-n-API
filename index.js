e69e439eb8b765dc95648a442ff3ad7356a8aea5
PedalConny <pedalconny@hotmail.com>
2024-01-23 11:26:51 +0100

    added start: npm run dev in json scripts

---
+++ index.js
@@ -0,0 +1,57 @@
+import express from "express";
+import { engine } from 'express-handlebars';
+import fs from "fs/promises";
+
+const app = express();
+app.engine('handlebars', engine());
+
+app.set('view engine', 'handlebars');
+
+app.set('views', './templates');
+
+
+const MENU = [
+  {
+    label: 'Home page',
+    link: '/',
+  },
+  {
+    label: 'About us',
+    link: '/aboutUs',
+  },
+  {
+    label: 'Movies',
+    link: '/movies',
+  }, 
+];
+
+
+async function renderPage(response, page) {
+  const currentPath = (page == 'index') ? '/' : `/${page}`;
+
+  response.render(page, {
+    menuItems: MENU.map(item => {
+      return {
+        active: currentPath == item.link,
+        label: item.label,
+        link: item.link,
+      };
+    }) 
+  });
+}
+
+
+app.get('/', async (request, response) => {
+  renderPage(response, 'index');
+});
+
+app.get('/aboutUs', async (request, response) => {
+  renderPage(response, 'aboutUs');
+});
+
+app.get('/movies', async (request, response) =>{
+  renderPage(response, 'movies');
+});
+
+app.use("/static", express.static("./static"));
+app.listen(5080);
