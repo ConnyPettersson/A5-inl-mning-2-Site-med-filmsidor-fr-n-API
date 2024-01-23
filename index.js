cfd5795d2c41068e356efa71ad59c920eb7996ac
PedalConny <pedalconny@hotmail.com>
2024-01-23 12:22:41 +0100

    added movies.js, installed node-fetch, templates home and movie handlebars

--- index.js
+++ index.js
@@ -1,14 +1,11 @@
 import express from "express";
 import { engine } from 'express-handlebars';
 import fs from "fs/promises";
-
+import {loadMovie, loadMovies} from "./src/movies.js";
 const app = express();
 app.engine('handlebars', engine());
-
 app.set('view engine', 'handlebars');
-
 app.set('views', './templates');
-
 
 const MENU = [
   {
@@ -40,6 +37,15 @@ async function renderPage(response, page) {
   });
 }
 
+app.get("/", async (request, response) => {
+  const movies = await loadMovies();
+  response.render("home", {movies});
+});
+
+app.get("/movies/:movieId", async (request, response) => {
+  const movie = await  loadMovie(request.params.movieId);
+  response.render("movie", {movie});
+});
 
 app.get('/', async (request, response) => {
   renderPage(response, 'index');
