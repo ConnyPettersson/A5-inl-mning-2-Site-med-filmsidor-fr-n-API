f7c7e82217166a1696cee43b43058c8bcd3d44fa
PedalConny <pedalconny@hotmail.com>
2024-01-26 14:31:43 +0100

    added test All movie titles show correct titles

--- tests/movies.spec.js
+++ tests/movies.spec.js
@@ -1,11 +1,17 @@
 import { expect, test } from '@jest/globals';
 import request from 'supertest';
 import app from '../src/app';
+import { loadMovies } from '../src/movies.js';
 
-test('Encanto page shows title of movie', async () => {
-  const response = await request(app).get('/movies/2')
-    .expect('Content-Type', 'text/html; charset=utf-8')
-    .expect(200);
+test('All movie pages show correct title', async () => {
+  const movies = await loadMovies();
 
-  expect(response.text).toMatch('Encanto');
+  for (const movie of movies) {
+    const response = await request(app)
+      .get(`/movies/${movie.id}`)
+      .expect('Content-Type', 'text/html; charset=utf-8')
+      .expect(200);
+
+    expect(response.text).toMatch(movie.title);
+  }
 });
